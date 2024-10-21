c961255ac8e480945c97872629296d87b4324a7a
Divyanshu Kaushal <123375624+Divyanshu2125@users.noreply.github.com>
2024-10-21 14:47:01 +0530

    Update footer.jsx

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -12,7 +12,7 @@ function Footer() {
         </div>
         <div className="flex flex-col md:flex-row items-center justify-between">
           <p className="text-sm">
-            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/abu-said-bd/" className="text-[#16f2b3]">Divyanshu Kaushal</Link>
+            © Developer Portfolio by <Link target="_blank" href="https://www.linkedin.com/in/divyanshu-kaushal-80b0a9262/" className="text-[#16f2b3]">Divyanshu Kaushal</Link>
           </p>
         </div>
       </div>
